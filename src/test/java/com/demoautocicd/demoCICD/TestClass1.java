f8ee9a89eda81ea5890e8ddde7bd37df41890f7d
hmwe pwint phyu <hmwepwintphyu@2c2p.com>
2021-12-30 09:43:00 +0630

    setup grid

--- src/test/java/com/demoautocicd/demoCICD/TestClass1.java
+++ src/test/java/com/demoautocicd/demoCICD/TestClass1.java
@@ -1,8 +1,15 @@
 package com.demoautocicd.demoCICD;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,12 +18,24 @@ public class TestClass1 {
 	public static WebDriver webdriver;
 	
 	@BeforeMethod
-  public void launchDriver() {
-	  System.setProperty("webdriver.chrome.driver","D:\\Demo-Eclipse\\demoCICD\\drivers\\chromedriver.exe");
-      ChromeOptions options = new ChromeOptions();
-      options.addArguments("--disable-notification");
-      webdriver= new ChromeDriver(options);
-      webdriver.manage().window().maximize();
+  public void launchDriver() throws MalformedURLException {
+//	  System.setProperty("webdriver.chrome.driver","D:\\Demo-Eclipse\\demoCICD\\drivers\\chromedriver.exe");
+//      ChromeOptions options = new ChromeOptions();
+//      options.addArguments("--disable-notification");
+//      webdriver= new ChromeDriver(options);
+//      webdriver.manage().window().maximize();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setCapability("browserName", "chrome");
+		chromeOptions.setCapability("platform", "WIN10");
+		chromeOptions.setCapability("platformName", "WINDOWS");
+		chromeOptions.setCapability("seleniumProtocol", "WebDriver");
+		chromeOptions.setCapability("maxInstances", "5");
+		webdriver = new RemoteWebDriver(new URL("http://192.168.142.107:4444/wd/hub"), chromeOptions);
+		webdriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		webdriver.manage().window().maximize();
+//		webdriver = new RemoteWebDriver(new URL("http://192.168.0.113:4444/wd/hub"), dr);
+//		System.setProperty("webdriver.chrome.driver","D:\\Demo-Eclipse\\demoCICD\\drivers\\chromedriver.exe");
+		
   }
   @Test
   public void Test1() {
